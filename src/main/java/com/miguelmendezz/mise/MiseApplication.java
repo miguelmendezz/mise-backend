1327e9b7010ec16aed6d45bfbd111d855db52ceb
miguelmendezz <miguel.mendes201@gmail.com>
2026-08-18 23:16:17 -0300

    chore: setup project structure and docker compose with PostgreSQL

---
+++ src/main/java/com/miguelmendezz/mise/MiseApplication.java
@@ -0,0 +1,13 @@
+package com.miguelmendezz.mise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiseApplication.class, args);
+	}
+
+}

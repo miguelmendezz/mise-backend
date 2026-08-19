7d51186265f9bc93a6135c6102a1b0ce3d584851
miguelmendezz <miguel.mendes201@gmail.com>
2026-08-19 00:09:27 -0300

    feat: add Supplier entity

---
+++ src/main/java/com/miguelmendezz/mise/entity/Supplier.java
@@ -0,0 +1,42 @@
+package com.miguelmendezz.mise.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Supplier {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String contact;
+
+    public Supplier() {
+    }
+
+    public Supplier(String name, String contact) {
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}

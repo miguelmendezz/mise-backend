9e36bc0e244db0919ec170ca31b9ab653cbe8f8e
miguelmendezz <miguel.mendes201@gmail.com>
2026-08-20 18:00:57 -0300

    feat: add Product entity with Supplier relationship

---
+++ src/main/java/com/miguelmendezz/mise/entity/Product.java
@@ -0,0 +1,61 @@
+package com.miguelmendezz.mise.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    private Supplier supplier;
+    private String name;
+    private String category;
+    private int stockQuantity;
+
+    public Product() {
+    }
+
+    public Product(String name, String category, int stockQuantity) {
+        this.name = name;
+        this.category = category;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+}
